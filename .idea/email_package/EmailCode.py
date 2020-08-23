2692c78d842e130948e1add968a8ed7848f51ef8
roger-mayer <rmayer1984@gmail.com>
2020-08-22 22:06:34 -0500

    refactored to EmailMessage function and added ability to add attachments

--- .idea/email_package/EmailCode.py
+++ .idea/email_package/EmailCode.py
@@ -3,37 +3,44 @@ import os
 
 # import smtp library module
 import smtplib
-import imghdr
 import EmailCreds
-# from email.message import EmailMessage
+import imghdr
+from email.message import EmailMessage
 
+# TO GET FROM OS
 # EMAIL_ADD = os.environ.get('EMAIL_ADDRESS')
 # EMAIL_PASS = os.environ.get('EMAIL_PASSWORD')
 
+# OR RUN IN TERMINAL FOR LOCAL DEBUGGING
+# python3 -m smtpd -c DebuggingServer -n localhost:1025
 
+# USED .gitignore TO HIDE PW EmailCreds
 EMAIL_ADD = EmailCreds.EMAIL_ADD
 EMAIL_PASS = EmailCreds.EMAIL_PASS
 print(EMAIL_ADD)
-print(EMAIL_PASS)
 
+msg = EmailMessage()
+msg['Subject'] = 'I sent this with python'
+msg['From'] = EMAIL_ADD
+msg['To'] = 'rmayer1984@gmail.com'
+msg.set_content('Hi! How are you!')
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    # identify as encripted connection
-    smtp.ehlo()
-    # encript traffic
-    smtp.starttls()
-    # reidentify self as encripted package
-    smtp.ehlo()
+files = ['RogerImg.jpeg', 'GeekdomSticker.jpeg']
+
+for file in files:
+    with open(file, 'rb') as f:
+        file_data = f.read()
+        file_type = imghdr.what(f.name)
+        file_name = f.name
+
+    msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+
 
     smtp.login(EMAIL_ADD, EMAIL_PASS)
+    smtp.send_message(msg)
 
-    subject = 'I sent this with python'
-    body = 'This is a plain text email'
-
-    msg = f'Subject: {subject}\n\n{body}'
-
-    smtp.sendmail(EMAIL_ADD, 'rmayer1984@gmail.com', msg)
-#
 # contacts = ['rmayer1984@gmail.com', 'rmayer1984@gmail.com']
 #
 # msg = EmailMessage()
